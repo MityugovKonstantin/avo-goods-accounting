cbbb1dde0bfa6f95c50e228eec1348bd7e44911e
Mityugov Konstantin <alxmira@yandex.ru>
2023-05-03 23:36:11 +0700

    Create a redirect
    Adding a create_ups file and auto-completing select objects on the page

--- src/main/java/ru/mityugov/avogoodsaccounting/ups/api/UpsController.java
+++ src/main/java/ru/mityugov/avogoodsaccounting/ups/api/UpsController.java
@@ -1,13 +1,16 @@
 package ru.mityugov.avogoodsaccounting.ups.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ru.mityugov.avogoodsaccounting.ups.models.Ups;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -15,10 +18,32 @@ import java.util.Map;
 public class UpsController {
 
     @GetMapping()
-    public ModelAndView index() {
-        Map<String, String> model = new HashMap<>();
-        model.put("name", "Alexey");
-        return new ModelAndView("index", model);
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping(path = "/create")
+    public String create(Model model) {
+        List<String> persons = new ArrayList<>();
+        persons.add("Nick");
+        persons.add("Bob");
+        persons.add("May");
+
+        List<String> locations = new ArrayList<>();
+        locations.add("ФЭТ");
+        locations.add("РК");
+        locations.add("ГК");
+
+        List<String> statuses = new ArrayList<>();
+        statuses.add("Работает");
+        statuses.add("Не работает");
+        statuses.add("В ремонте");
+
+        model.addAttribute("persons", persons);
+        model.addAttribute("locations", locations);
+        model.addAttribute("statuses", statuses);
+
+        return "create_ups";
     }
 
     @PostMapping()
